d2516f9a3d9d0cb0638f819f94473268489266f4
tony9701 <leesin9701@gmail.com>
2024-07-12 18:20:07 +0300

    project and templates creation
    Created the project and some of the templates

---
+++ src/main/java/com/topHomes/propertiesApp/PropertiesAppApplication.java
@@ -0,0 +1,13 @@
+package com.topHomes.propertiesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topHomes/propertiesApp/PropertiesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topHomes.propertiesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
